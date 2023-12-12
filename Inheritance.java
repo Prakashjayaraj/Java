4d60f4acfec5b03f3912e044124be466206e4aed
Prakashjayaraj <151844782+Prakashjayaraj@users.noreply.github.com>
2023-12-12 15:27:55 +0530

    Update Inheritance.java
    Build a basic banking system using inheritance. Define a base class Account with properties like accountNumber, balance, and methods like deposit and withdraw. Extend this class to create subclasses like SavingsAccount and CheckingAccount with specific functionalities.

--- Inheritance.java
+++ Inheritance.java
@@ -1,30 +1,80 @@
 package Oops;
-class a{
-    public void a(){
-        System.out.println("this is a class");
-    }
-}
-class b extends  a{
+class Account {
+    private String accountNumber;
+    protected double balance;
 
-    public void b() {
-        System.out.println("this is b cllass");
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: $" + amount);
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Insufficient balance.");
+        }
+    }
+
+    public void displayBalance() {
+        System.out.println("Account Balance: $" + balance);
     }
 }
-class c extends a{
-    public void c (){
-        System.out.println("this is c class");
+class SavingsAccount extends Account {
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    public void addInterest() {
+        double interest = balance * interestRate / 100;
+        deposit(interest);
+        System.out.println("Added interest: $" + interest);
     }
 }
+
+
+class CheckingAccount extends Account {
+    private double overdraftLimit;
+
+    public CheckingAccount(String accountNumber, double balance, double overdraftLimit) {
+        super(accountNumber, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (balance + overdraftLimit >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Exceeds overdraft limit.");
+        }
+    }
+}
+
+
+
 public class Inheritance {
     public static void main(String[] args) {
-        c obj = new c();
-b ooo = new b();
-ooo.a();
-ooo.b();
-        obj.a();
-        obj.c();
-
-
-
+        SavingsAccount savings = new SavingsAccount("SAV001", 1000, 5);
+        savings.displayBalance(); 
+        savings.deposit(500);
+        savings.displayBalance();
+        savings.addInterest(); 
+        savings.displayBalance(); 
+        CheckingAccount checking = new CheckingAccount("CHK001", 2000, 1000);
+        checking.displayBalance(); 
+        checking.withdraw(1500); 
+        checking.displayBalance(); 
+        checking.withdraw(2000); 
     }
-}
+    }
